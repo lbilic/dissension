414d1ce7a84e164376280cc9d275185f61828c11
shocker <phoenix-023@hotmail.com>
2021-09-07 16:09:52 +0200

    Removed reduntant encoding

--- server.py
+++ server.py
@@ -32,7 +32,9 @@ def clientthread(conn, addr):
                     connected_user = message.decode().split(HANDSHAKE_MESSAGE)[0].strip()
                     if connected_user != client_names:
                         client_names.append(connected_user)
-                        broadcast_message(str(';'.join(client_names).join(HANDSHAKE_MESSAGE)).encode())
+                        print(client_names)
+                        userlist = ';'.join(client_names) + HANDSHAKE_MESSAGE
+                        broadcast_message(userlist, conn)
                 else:
                     print("<" + addr[0] + ">" + message.decode())
                     message_to_send = message.decode()
