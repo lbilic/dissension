a2ef60fe83d21947c4486d7c3059406c670b6deb
Markidane <kojicmarko96@gmail.com>
2021-09-08 14:56:28 +0200

    Link makeover

--- link.py
+++ link.py
@@ -6,26 +6,24 @@ class Link:
         self.mainapp = mainapp
         self.root = root
         # Main frame configuration
-        self.mainframe = ttk.Frame(root, padding="3 3 12 12")
-        self.mainframe.grid(column=1, row=0, sticky=(N, W, E, S))
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
+        self.mainframe = ttk.Frame(root, padding=("75 35 75 35"))
+        self.mainframe.grid(column=0, row=0)
 
         # Server address
-        Label(self.mainframe, text="Server address: ").grid(column=0, row=0, sticky=(W))
+        Label(self.mainframe, text="Server address: ").grid(column=0, row=0)
         self.server_ip = StringVar()
-        server_entry = ttk.Entry(self.mainframe, width=20, textvariable=self.server_ip)
-        server_entry.grid(column=3, row=0, sticky=(W, E))
+        server_entry = ttk.Entry(self.mainframe, width=25, textvariable=self.server_ip)
+        server_entry.grid(column=0, row=1)
 
         # Nickname
-        Label(self.mainframe, text="Nickname: ").grid(column=0, row=1, sticky=(W))
+        Label(self.mainframe, text="Nickname: ").grid(column=0, row=2)
         self.nickname = StringVar()
-        nickname_entry = ttk.Entry(self.mainframe, width=20, textvariable=self.nickname)
-        nickname_entry.grid(column=3, row=1, sticky=(W, E))
+        nickname_entry = ttk.Entry(self.mainframe, width=25, textvariable=self.nickname)
+        nickname_entry.grid(column=0, row=3)
 
-        Button(self.mainframe, text="Connect", command=self.connect).grid(column=1, row=3, sticky=(S))
+        Button(self.mainframe, text="Connect", command=self.connect).grid(column=0, row=5)
 
-        for child in self.mainframe.winfo_children(): 
+        for child in self.mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5)
 
         server_entry.focus()
@@ -35,7 +33,7 @@ class Link:
         nickname = self.nickname.get()
         nickname_len = len(nickname)
         if not nickname.isalnum() or nickname_len < 2 or nickname_len > 16:
-            Label(self.mainframe, text="Nickname must be between 2 and 16 characters in length.", fg="red").grid(column=3, row=2, sticky=(E))
+            Label(self.mainframe, text="Nickname must be between 2 \n and 16 characters in length.", fg="red").grid(column=0, row=4)
         else:
             self.root.destroy()
             self.mainapp.setup(self.server_ip.get(), self.nickname.get())
